694285116bc1b042e299ce8cf1431ce0884ea986
otmallah <othmanmallah15@gmail.com>
2023-01-01 16:34:47 +0100

    bismilah bdina map

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: otmallah <otmallah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 01:54:59 by otmallah          #+#    #+#             */
-/*   Updated: 2022/12/31 20:49:41 by otmallah         ###   ########.fr       */
+/*   Updated: 2023/01/01 16:34:01 by otmallah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include "stack/stack.hpp"
 #include <sys/time.h>
 #include <vector>
+#include <map>
 #include <iostream>
 #include <iterator>
 #include <ctime>
@@ -33,41 +34,8 @@
 #include <sys/time.h>
 #include "containers_test/srcs/base.hpp"
 
-#define TESTED_TYPE std::string
-
-void	checkErase(TESTED_NAMESPACE::vector<TESTED_TYPE> const &vct,
-					TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator const &it)
-{
-	static int i = 0;
-	std::cout << "[" << i++ << "] " << "erase: " << it - vct.begin() << std::endl;
-	printSize(vct);
-}
 
 int main(void)
 {
-	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(10);
-
-	for (unsigned long int i = 0; i < vct.size(); ++i)
-		vct[i] = std::string((vct.size() - i), i + 65);
-	printSize(vct);
-
-	checkErase(vct, vct.erase(vct.begin() + 2));
-
-	checkErase(vct, vct.erase(vct.begin()));
-	checkErase(vct, vct.erase(vct.end() - 1));
-
-	checkErase(vct, vct.erase(vct.begin(), vct.begin() + 3));
-	checkErase(vct, vct.erase(vct.end() - 3, vct.end() - 1));
-
-	vct.push_back("Hello");
-	vct.push_back("Hi there");
-	printSize(vct);
-	checkErase(vct, vct.erase(vct.end() - 3, vct.end()));
-
-	vct.push_back("ONE");
-	vct.push_back("TWO");
-	vct.push_back("THREE");
-	vct.push_back("FOUR");
-	printSize(vct);
-	checkErase(vct, vct.erase(vct.begin(), vct.end()));
+		
 }
