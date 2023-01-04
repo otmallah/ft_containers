24f7bfd9ba2319547821c369dfc347b9ba016b21
otmallah <othmanmallah15@gmail.com>
2023-01-04 16:38:14 +0100

    saring BS implemenaion

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -6,7 +6,7 @@
 /*   By: otmallah <otmallah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 21:11:58 by otmallah          #+#    #+#             */
-/*   Updated: 2023/01/03 19:54:37 by otmallah         ###   ########.fr       */
+/*   Updated: 2023/01/04 16:35:13 by otmallah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,70 @@
 #include <iostream>
 #include <map>
 
+class bst
+{
+    public:
+    bst *left;
+    bst *right;
+    int _data;
+    bst (int data)
+    {
+        _data = data;
+        left = NULL;
+        right =  NULL;
+    }
+};
+
+bst *creat(int data)
+{
+    bst *root = new bst(data);
+    return root;
+}
+
+bst    *search(bst *root, int data)
+{
+    if (data < root->_data)
+        search(root->left, data);
+    else if (data > root->_data)
+        search(root->right, data);
+    return root;
+}
+
+bst*    insert(bst *root, int data)
+{
+    bst *mp = root;
+    if (mp == NULL)
+    {
+        mp = creat(20);
+        return root;
+    }
+    while (mp)
+    {
+        if (data > mp->_data)
+            mp = mp->right;
+        else if (data < mp->_data)
+            mp = mp->left;
+        else
+        {
+            mp = creat(data);
+            return root;
+        }
+    }
+    return root;
+}
+
+void inorder(bst *tmp)
+{
+    if (tmp == NULL)
+        return;
+    inorder(tmp->left);
+    std::cout << tmp->_data << std::endl;
+    inorder(tmp->right);
+}
+
 int main()
 {
-
-    std::map<std::string, int> map;
-
-    map["one"] = 1;
-    map["two"] = 2;
-    map["three"] = 3;
-
-    std::map<std::string, int>::iterator it = map.begin();
-
-    while (it != map.end())
-    {
-        std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
-        ++it;
-    }
-
-    return 0;
+    bst *root  = NULL;
+    root = insert(root, 20);
+    s
 }
