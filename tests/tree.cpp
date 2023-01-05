3de5fec1e2d0f83bf60445f4d596621b92ffd223
otmallah <othmanmallah15@gmail.com>
2023-01-05 15:15:12 +0100

    insert in BST done

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -6,7 +6,7 @@
 /*   By: otmallah <otmallah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 21:11:58 by otmallah          #+#    #+#             */
-/*   Updated: 2023/01/04 16:35:13 by otmallah         ###   ########.fr       */
+/*   Updated: 2023/01/05 15:13:17 by otmallah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ class bst
     bst *left;
     bst *right;
     int _data;
+    bst() {}
     bst (int data)
     {
         _data = data;
@@ -30,56 +31,104 @@ class bst
     }
 };
 
-bst *creat(int data)
+bst *create(int data)
 {
     bst *root = new bst(data);
     return root;
 }
 
-bst    *search(bst *root, int data)
-{
-    if (data < root->_data)
-        search(root->left, data);
-    else if (data > root->_data)
-        search(root->right, data);
-    return root;
-}
-
 bst*    insert(bst *root, int data)
 {
-    bst *mp = root;
-    if (mp == NULL)
+    if (root == NULL)
+        return root = create(data);
+    bst *prev =  NULL;
+    bst *temp = root;
+    while (root != NULL)
     {
-        mp = creat(20);
-        return root;
-    }
-    while (mp)
-    {
-        if (data > mp->_data)
-            mp = mp->right;
-        else if (data < mp->_data)
-            mp = mp->left;
+        prev = root;
+        if (data > root->_data)
+            root = root->right;
+        else if (data < root->_data)
+            root = root->left;
         else
-        {
-            mp = creat(data);
             return root;
-        }
     }
-    return root;
+    if (data > prev->_data)
+        prev->right = create(data);
+    else if (data < prev->_data)
+        prev->left = create(data);
+    return temp;
 }
 
-void inorder(bst *tmp)
+bool    search(bst* root, int data)
 {
-    if (tmp == NULL)
-        return;
-    inorder(tmp->left);
-    std::cout << tmp->_data << std::endl;
-    inorder(tmp->right);
+    if (root == NULL)
+        std::cout << "empty bst\n";
+    while (root)
+    {
+        if (root->_data == data)
+        {
+            return true;
+            break;
+        }
+        if (data > root->_data)
+            root = root->right;
+        else if (data < root->_data)
+            root = root->left;
+    }
+    return false;
+}
+
+void      print_mini(bst *root)
+{
+    while (root)
+    {
+        if (!root->left)
+            std::cout << root->_data << std::endl;
+        root = root->left;
+    }
+}
+
+void      print_max(bst *root)
+{
+    while (root)
+    {
+        if (!root->right)
+            std::cout << root->_data << std::endl;
+        root = root->right;
+    }
+}
+
+// int sizeBST(bst* root)
+// {
+    
+// }
+
+void    printInorder(bst *root)
+{
+    while (root)
+    {
+        
+    }
+    
 }
 
 int main()
 {
-    bst *root  = NULL;
-    root = insert(root, 20);
-    s
+    bst *root = NULL;
+    root = insert(root, 10);
+    root = insert(root, 8);
+    root = insert(root, 12);
+    root = insert(root, 7);
+    root = insert(root, 9);
+    root = insert(root, 11);
+    root = insert(root, 14);
+
+    std::cout << root->right->right->_data << std::endl;
+    // printInorder(root);
+    // search(root, 22000);
+    // std::cout << "max value in BST : " ;
+    // print_max(root);
+    // std::cout << "min value in BST : ";
+    // print_mini(root);
 }
